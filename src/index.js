2d7aea0bd52122718915ab2e0bd66379926efb2e
Brian Mckie <brianmckie2@gmail.com>
2017-06-04 16:15:59 -0400

    Adding custom components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import marked from 'marked';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//Input Component
+class Input extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      searchTerms: ''
+    }
+  };
+    handleInputChange(e){
+      this.setState({
+        searchTerms: e.target.value
+       
+      });
+       //console.log('search Terms ' + searchTerms);
+    }
+   handleOutput(searchTerms){
+    //API call
+    
+ console.log(marked('I am using 00121'));
+  }
+   render(){
+     return(
+      <div>
+         <textarea onClick={this.handleOutput.bind(this)} cols="40" rows="5"></textarea>
+      
+      </div>
+     );
+   }
+  
+}
+
+//Marked Component
+class MarkedOutput extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      output: ''
+    };
+  }
+  
+//   handleInput(searchTerms){
+//     //API call
+    
+//     axios.get('https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=dog&limit=10').then(response => {
+//       console.log(response);
+//     });
+//   }
+}
+
+
+ReactDOM.render(<Input />, document.getElementById('app'));
